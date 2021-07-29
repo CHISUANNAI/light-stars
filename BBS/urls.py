6517686757a2618622be272234a615a98f230af9
CHISUANNAI <813728594@qq.com>
2021-07-29 20:32:24 +0800

    ryh

--- BBS/urls.py
+++ BBS/urls.py
@@ -8,5 +8,6 @@ from BBS import comment, views
 urlpatterns = [
    # path('list_BBS_content/',views.dispatcher)
    path('list_BBS_content',views.dispatcher),
-   path('comment',comment.dispatcher)
+   path('comment',comment.dispatcher),
+   path("open_post",views.open_post,name='open_post')
 ]
